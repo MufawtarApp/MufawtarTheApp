4531b708af329acdfec5d43272f12e9e6ce0f56b
talkebsi <talalalkebsi34@gmail.com>
2023-12-02 20:30:10 +0300

    Add a condition password must be complex close #5

--- Mufawtar/app/src/main/java/com/example/mufawtar/signUp.java
+++ Mufawtar/app/src/main/java/com/example/mufawtar/signUp.java
@@ -12,9 +12,10 @@ import android.widget.Toast;
 
 public class signUp extends AppCompatActivity {
 
-    EditText username , password , repassword;
-    Button  signup ;
+    EditText username, password, repassword;
+    Button signup;
     DBHelper DB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,35 +33,34 @@ public class signUp extends AppCompatActivity {
                 String user = username.getText().toString();
                 String pass = password.getText().toString();
                 String repass = repassword.getText().toString();
-                if (TextUtils.isEmpty(user) || TextUtils.isEmpty(pass) || TextUtils.isEmpty(repass))
-                    Toast.makeText( signUp.this, "All fields Required", Toast.LENGTH_SHORT).show();
-                else {
-                    if (pass.equals(repass)) {
-                        Boolean checkuser = DB.checkusernane(user);
-                        if (checkuser == false) {
-                            Boolean insert = DB.insertData(user, pass);
-                            if (insert == true) {
-                                Toast.makeText(signUp.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(), homeActivity.class);
-                                startActivity(intent);
 
-                            } else {
-                                Toast.makeText(signUp.this, "Registration Failed", Toast.LENGTH_SHORT).show();
-                            }
-                        }else {
-                            Toast.makeText(signUp.this, "User already Exists", Toast.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(user) || TextUtils.isEmpty(pass) || TextUtils.isEmpty(repass)) {
+                    Toast.makeText(signUp.this, "All fields Required", Toast.LENGTH_SHORT).show();
+                } else if (!isPasswordComplex(pass)) {
+                    Toast.makeText(signUp.this, "Password must include numbers, letters, upper and lower case, and be at least 8 characters long", Toast.LENGTH_LONG).show();
+                } else if (!pass.equals(repass)) {
+                    Toast.makeText(signUp.this, "Passwords are not matching", Toast.LENGTH_SHORT).show();
+                } else {
+                    Boolean checkuser = DB.checkusernane(user);
+                    if (!checkuser) {
+                        Boolean insert = DB.insertData(user, pass);
+                        if (insert) {
+                            Toast.makeText(signUp.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), homeActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(signUp.this, "Registration Failed", Toast.LENGTH_SHORT).show();
                         }
-                        }else{
-                                Toast. makeText(signUp.this,  "Passwords are not matching", Toast. LENGTH_SHORT).show();
-                            }
-                        }
+                    } else {
+                        Toast.makeText(signUp.this, "User already Exists", Toast.LENGTH_SHORT).show();
                     }
-
-
+                }
+            }
         });
-
-
-
-        }
-
     }
+
+    private boolean isPasswordComplex(String password) {
+        String pattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$";
+        return password.matches(pattern);
+    }
+}
